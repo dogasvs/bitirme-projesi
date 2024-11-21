08a71fe063ce24bda1cdf18b0b5d5d8eef1d3ef3
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-11-21 17:35:01 +0300

    FAQsa css eklendi

--- app/faq/page.jsx
+++ app/faq/page.jsx
@@ -1,10 +1,48 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import "./faq.css"
+import DiamondSvg from "@/svgs/diamond";
 
 export default function FAQ() {
   return (
-    <div className="faq">
+    <div className="faqContainer">
       <Header />
+      <div className="faq">
+        <details>
+          <summary>Siparişimi nasıl takip edebilirim?  <DiamondSvg /></summary>
+          <p>Siparişinizin durumu hakkında bilgi almak için hesabınıza giriş yaparak "Siparişlerim" sekmesini ziyaret edebilir veya size gönderilen <br /> takip numarasını kullanarak kargo firması üzerinden takip yapabilirsiniz.</p>
+        </details>
+
+        <details>
+          <summary>İade ve değişim süreci nasıl işler?  <DiamondSvg /></summary>
+          <p>Ürünlerimizi teslim aldıktan sonra 14 gün içerisinde iade veya değişim talebinde bulunabilirsiniz. <br /> Ürünlerin kullanılmamış ve orijinal ambalajında olması gerekmektedir. Daha fazla bilgi için "İade ve Değişim Politikası" sayfamızı inceleyebilirsiniz.</p>
+        </details>
+
+        <details>
+          <summary>Özel tasarım siparişleri nasıl verilir?  <DiamondSvg /></summary>
+          <p>Size özel tasarımlar oluşturmak için bizimle iletişime geçebilirsiniz. Taleplerinizi mağazalarımızda ya da online form aracılığıyla <br /> iletebilirsiniz. Tasarım sürecimiz, ihtiyaçlarınıza göre şekillenir ve size özgü bir ürün ortaya çıkar.</p>
+        </details>
+
+        <details>
+          <summary>Hangi ödeme yöntemlerini kabul ediyorsunuz?  <DiamondSvg /></summary>
+          <p>Kredi kartı, banka kartı, kapıda ödeme ve online ödeme sistemlerini kullanabilirsiniz. Ödeme seçeneklerimiz güvenli ve hızlıdır.</p>
+        </details>
+
+        <details>
+          <summary>Takılarımın bakımını nasıl yapabilirim?  <DiamondSvg /></summary>
+          <p>Takılarınızı uzun süre kullanabilmek için düzenli olarak temizleyin ve kimyasallardan uzak tutun. Daha detaylı bilgi ve profesyonel bakım <br /> için mağazalarımızı ziyaret edebilirsiniz.</p>
+        </details>
+
+        <details>
+          <summary>Siparişlerim ne kadar sürede teslim edilir?  <DiamondSvg /></summary>
+          <p>Siparişlerimiz genellikle 2-5 iş günü içerisinde teslim edilir. Özel tasarımlar ve kişiselleştirilmiş ürünler için teslimat süresi farklılık <br /> gösterebilir.</p>
+        </details>
+
+        <details>
+          <summary>Size nasıl ulaşabilirim? <DiamondSvg /></summary>
+          <p>Bizimle iletişim sayfamız üzerinden ya da mağazalarımızı ziyaret ederek kolayca iletişime geçebilirsiniz.</p>
+        </details>
+      </div>
       <Footer />
     </div>
   )
---
+++ svgs/diamond.jsx
@@ -0,0 +1,7 @@
+export default function DiamondSvg() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" clipRule="evenodd" viewBox="0 0 500 500" id="star">
+      <path fill="#ffd600" d="M250,25L317.308,182.692L442.308,250L317.308,317.308L250,475L182.692,317.308L57.692,250L182.692,182.692L250,25Z"></path>
+    </svg>
+  )
+}
