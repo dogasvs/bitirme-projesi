61fc68a2ab09acf9b65a32b65b6342030cec215c
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-12-01 21:56:04 +0300

    headerdaki sepette ne kadar ürün olduğu getirildi

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -4,8 +4,19 @@ import DropdownUser from "../dropdown";
 import AlisverisSepetiSvg from "@/svgs/basket";
 import SearchSvg from "@/svgs/search";
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
 
-export default function Header() {
+export default async function Header() {
+  const supabase = await createClient();
+  const { data: { user }, error: userError } = await supabase.auth.getUser();
+
+  const { data: cartItems, error: cartError } = await supabase
+    .from("cart")
+    .select("quantity")
+    .eq("user_id", user.id);
+
+  const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <div className="headerContainer">
       <div className="headerBrand">
@@ -15,8 +26,9 @@ export default function Header() {
       <div className="profile">
         <SearchSvg />
         <DropdownUser />
-        <Link href={'/cart-detail'}>
+        <Link href={'/cart-detail'} className="cartLink">
           <AlisverisSepetiSvg />
+          <span className="cartBadge">{cartCount}</span>
         </Link>
       </div>
     </div>
