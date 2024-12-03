8fe1745d7285c7c0ba3d86be5b1b918f11a8cc36
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-12-04 01:51:53 +0300

    add-to-carta modal eklendi cssi yapılıd categroies p css eklendi svgler değiştirildi cart-detail css eklndi

--- components/add-to-cart/index.jsx
+++ components/add-to-cart/index.jsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { createClient } from "@/utils/supabase/client";
+import Link from "next/link";
+import "./addToCart.css"
+import { useState } from "react";
+import CloseSvg from "@/svgs/close";
 
 export function AddToCartButton({ productId }) {
+  const [showModal, setShowModal] = useState(false);
+
   const addToCart = async () => {
     const supabase = await createClient();
     const { data: { user }, error: userError } = await supabase.auth.getUser();
@@ -14,12 +20,40 @@ export function AddToCartButton({ productId }) {
     .eq("id", productId)
     .single(); 
 
-    const { error } = await supabase
+    const { data: cart, error: cartError } = await supabase
       .from("cart") 
       .insert([
       { user_id: userId, product_id: productId, quantity: 1, price: product.price  }]);
-      window.location.reload();
+
+      if (cartError) {
+        console.log("Sepete ekleme sırasında bir hata oluştu.");
+      } else {
+        setShowModal(true); 
+      }
   };
 
-  return <button className="addtoCart" onClick={addToCart}>Sepete Ekle</button>;
+  return (
+    <>
+      <button className="addtoCart" onClick={addToCart}>
+        Sepete Ekle
+      </button>
+
+      {showModal && (
+        <div className="modalBackdrop">
+          <div className="modalContent">
+            <div className="title">
+              <h3>Ürün sepete eklendi!</h3>
+              <button onClick={() => setShowModal(false)}> <CloseSvg /> </button>  
+            </div>
+            <div className="modalActions">
+              <button onClick={() => setShowModal(false)}>Alışverişe Devam Et</button>
+              <Link href="/cart-detail">
+                <button>Sepete Git</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
