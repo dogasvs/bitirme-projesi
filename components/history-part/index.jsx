24fedd12b2cfb894ca9ca4e32696ab11ea3b70ed
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-12-04 19:15:45 +0300

    shine.pngnin height değeri değiştirildi

--- components/history-part/index.jsx
+++ components/history-part/index.jsx
@@ -4,7 +4,7 @@ import "./history.css"
 export default function HistoryPart() {
   return (
     <div className="historyContainer">
-      <Image src={"/images/shine.png"} width={500} height={500} alt="vintage kolye" priority />
+      <Image src={"/images/shine.png"} width={500} height={600} alt="vintage kolye" priority />
       <div className="historyPartInfo">
         <h2>&apos;Sizin Hikayeniz, Sizin Takınız&apos;</h2>
         <p>Takılar, sadece bir süs eşyası değil; aynı zamanda geçmişi hatırlatan, duygularımızı ifade eden ve anılarımıza eşlik eden <br /> önemli birer parçadır. Geçmişin ihtişamından ilham alan, bugünün zarafetiyle şekillenen ve geleceğe ışık tutan takılarla <br /> siz de kendi hikayenizi yazabilirsiniz.</p>
