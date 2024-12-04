ae3f028a74d517336d4b0dbbdb78a16803ec80c6
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-12-04 18:57:03 +0300

    deploy için eslint hatası düzeltildi

--- components/history-part/index.jsx
+++ components/history-part/index.jsx
@@ -6,7 +6,7 @@ export default function HistoryPart() {
     <div className="historyContainer">
       <Image src={"/images/shine.png"} width={500} height={500} alt="vintage kolye" priority />
       <div className="historyPartInfo">
-        <h2>'Sizin Hikayeniz, Sizin Takınız'</h2>
+        <h2>&apos;Sizin Hikayeniz, Sizin Takınız&apos;</h2>
         <p>Takılar, sadece bir süs eşyası değil; aynı zamanda geçmişi hatırlatan, duygularımızı ifade eden ve anılarımıza eşlik eden <br /> önemli birer parçadır. Geçmişin ihtişamından ilham alan, bugünün zarafetiyle şekillenen ve geleceğe ışık tutan takılarla <br /> siz de kendi hikayenizi yazabilirsiniz.</p>
       </div>
     </div>
